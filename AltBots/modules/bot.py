c2f6eb03817db1b8a2e8a67d871441d70c4f9c83
@devil82 <153984431+Samyak1222@users.noreply.github.com>
2024-01-12 13:54:02 +0530

    Update bot.py

--- AltBots/modules/bot.py
+++ AltBots/modules/bot.py
@@ -24,7 +24,7 @@ async def ping(e):
         altron = await e.reply(f"» __ASHWANI__")
         end = datetime.now()
         mp = (end - start).microseconds / 1000
-        await altron.edit(f"__⚡️DEVIL⚡️__\n» `{mp} ᴍꜱ`")
+        await altron.edit(f"__⚡️DEVIL AA GAYA⚡️__\n» `{mp} ᴍꜱ`")
 
 
 @X1.on(events.NewMessage(incoming=True, pattern=r"\%sreboot(?: |$)(.*)" % hl))
